c57e9df42e07ecba8440599cb9f61ad587beb550
kuugy <54203454+kuugy@users.noreply.github.com>
2025-02-05 13:37:28 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,34 +1,19 @@
 // create web server
 // create web server for comments
-// 1. import express
-const express = require('express');
-// 2. create express server
-const app = express();
-// 3. create a port
-const port = 3000;
-// 4. create a route
-app.get('/', (req, res) => {
-    res.send('Hello world')
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+// create web server for comments
+app.get('/comments', function(req, res) {
+  fs.readFile('comments.json', function(err, data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
 });
-// 5. listen on a port
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-// 6. create a route for comments
-app.get('/comments', (req, res) => {
-    res.send('Comments page');
-});
-// 7. create a route for comments/:id
-app.get('/comments/:id', (req, res) => {
-    res.send('Comments id page');
-});
-// 8. create a route for comments/:id/:comment
-app.get('/comments/:id/:comment', (req, res) => {
-    res.send('Comments id comment page');
-});
-// 9. create a route for comments/:id/:comment/:reply
-app.get('/comments/:id/:comment/:reply', (req, res) => {
-    res.send('Comments id comment reply page');
-});
+
+app.listen(3000);
+console.log('Server started: http://localhost:3000/comments');
+
 
 
